4680d5ee1b5c928e4124c1c49243f29605588181
KasperBryde <kasperjacobsen17@gmail.com>
2024-02-27 13:01:30 +0100

    Abstract Unit class and stuff

---
+++ DDU2024KKM-RTS/Assets/Scripts/Unit.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour
+{
+    public int type;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO - make work!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO - make work!
+    }
+}
