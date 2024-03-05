84fea6ddd45f8b7c7baee5acfd03278706f47519
KasperBryde <kasperjacobsen17@gmail.com>
2024-03-05 13:05:23 +0100

    WaypointPlacer

---
+++ DDU2024KKM-RTS/Assets/Scripts/WaypointPlacer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointPlacer : MonoBehaviour
+{
+    public Transform waypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float mousePosY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(waypoint, new Vector2(mousePosX, mousePosY), Quaternion.identity);
+            //Debug.Log(waypoint.position);
+        }
+    }
+}
