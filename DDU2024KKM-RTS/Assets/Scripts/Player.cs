b0fd522bf45377608dad0675ad0b4c9edb7dc851
KasperBryde <kasperjacobsen17@gmail.com>
2024-03-12 13:17:19 +0100

    Unit marking system

--- DDU2024KKM-RTS/Assets/Scripts/Player.cs
+++ DDU2024KKM-RTS/Assets/Scripts/Player.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaypointPlacer : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public Transform waypoint;
-
+    public Transform mouse;
     public List<Transform> units; //testvalue - should be a list whose elements should be able to be chosen by the player
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Awake()
+    {
+        mouse = Instantiate(mouse, new Vector2(Screen.width, Screen.height), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -21,9 +26,24 @@ public class WaypointPlacer : MonoBehaviour
         float mousePosX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
         float mousePosY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
 
+        if (Input.GetMouseButton(0))
+        {
+            mouse.transform.localScale = new Vector2(mousePosX - mouse.transform.position.x, mouse.transform.position.y - mousePosY);
+        }
+        else
+        {
+            mouse.transform.position = new Vector2(mousePosX, mousePosY);
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            
+            mouse.transform.localScale = new Vector2(1, 1);
+        }
+
         if (!(units.Count == 0))
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(1))
             {
                 for (int i = 0; i < units.Count; i++)
                 {
