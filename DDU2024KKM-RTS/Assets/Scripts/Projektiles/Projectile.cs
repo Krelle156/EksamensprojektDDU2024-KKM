238718354d0272b1e9228a6f41250e30cf470ccb
Krelle Kanonfoder <kotofteng@gmail.com>
2024-04-02 13:58:33 +0200

    Smokeparticle optimization

--- DDU2024KKM-RTS/Assets/Scripts/Projektiles/Projectile.cs
+++ DDU2024KKM-RTS/Assets/Scripts/Projektiles/Projectile.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    protected float maxFlightDistance = 20;
+    protected float flightDistance = 10;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
+    }
+
+    public virtual void Launch(float dist)
+    {
+        maxFlightDistance = dist;
     }
 }
