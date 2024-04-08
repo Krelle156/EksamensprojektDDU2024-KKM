40e40703f79683cf98a749e0c57f8e39a944ca1e
Krelle Kanonfoder <kotofteng@gmail.com>
2024-04-08 10:27:02 +0200

    Corpse variation

--- DDU2024KKM-RTS/Assets/Scripts/Effects/ParticleManager.cs
+++ DDU2024KKM-RTS/Assets/Scripts/Effects/ParticleManager.cs
@@ -8,18 +8,24 @@ public class ParticleManager : MonoBehaviour
 
     [SerializeField] protected SmokeParticle smoke;
     [SerializeField] protected ExplosionEffect explosion;
-    [SerializeField] protected CorpseScript corpse;
+    [SerializeField] protected CorpseScript corpse1, corpse2;
 
-    protected static CorpseScript[] corpseList = new CorpseScript[20];
-    protected static int corpseCount;
+    protected static CorpseScript[] corpseList1 = new CorpseScript[20];
+    protected static CorpseScript[] corpseList2 = new CorpseScript[20];
+    protected static int corpseCount1, corpseCount2;
 
     protected 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i=0; i<corpseList.Length; i++)
+        for(int i=0; i<corpseList1.Length; i++)
         {
-            corpseList[i] = Instantiate(corpse, Vector2.zero, Quaternion.identity);
+            corpseList1[i] = Instantiate(corpse1, Vector2.zero, Quaternion.identity);
+        }
+
+        for (int i = 0; i < corpseList2.Length; i++)
+        {
+            corpseList2[i] = Instantiate(corpse2, Vector2.zero, Quaternion.identity);
         }
     }
 
@@ -31,8 +37,17 @@ public class ParticleManager : MonoBehaviour
 
     public static void spawnCorpse(Vector3 position, float t, float s)
     {
-        corpseList[corpseCount].spawn(position, t, s);
-        corpseCount++;
-        if (corpseCount >= corpseList.Length) corpseCount = 0;
+        if(Random.value<0.5f)
+        {
+            corpseList1[corpseCount1].spawn(position, t, s);
+            corpseCount1++;
+            if (corpseCount1 >= corpseList1.Length) corpseCount1 = 0;
+        } else
+        {
+            corpseList2[corpseCount2].spawn(position, t, s);
+            corpseCount2++;
+            if (corpseCount2 >= corpseList2.Length) corpseCount2 = 0;
+        }
+        
     }
 }
